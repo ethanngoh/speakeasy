ddde952f1115b2a5f7c803b920e420685d581bf9
Chris Bentivenga <chris@chrisbenti.com>
2022-09-04 14:17:11 -0700

    recipies

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import "./App.css";
 import { Menu } from "./pages/menu";
 
 import { HashRouter, Routes, Route } from "react-router-dom";
+import { Recipes } from "./pages/recipes";
 
 const App = () => {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Menu />} />
-        <Route path="about" element={<h1>STUFF</h1>} />
+        <Route path="recipes" element={<Recipes />} />
       </Routes>
     </HashRouter>
   );
---
+++ src/pages/recipes.tsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import { Cocktail } from "../model/cocktail";
+import * as drinks from "../data/cocktails";
+
+const DrinkContainer = styled.div`
+  margin-bottom: 1em;
+  border: 1px solid black;
+  padding: 1em;
+`;
+const DrinkTitle = styled.div`
+  font-weight: bold;
+`;
+
+const DrinkInstructions = ({ drink }: { drink: Cocktail }) => (
+  <DrinkContainer>
+    <DrinkTitle>{drink.name}</DrinkTitle>
+    <div>{drink.description}</div>
+    <div>
+      <div>Ingredients:</div>
+      <ul>
+        {drink.ingredients.map((ingredient) => (
+          <li key={ingredient.name}>
+            {"  "} - {ingredient.name} ({ingredient.value} {ingredient.unit})
+          </li>
+        ))}
+      </ul>
+    </div>
+    <div>
+      {drink.notes} ({drink.servingGlassType})
+    </div>
+  </DrinkContainer>
+);
+
+const RecipesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 600px;
+  margin: 0 auto;
+`;
+
+export const Recipes = () => (
+  <RecipesContainer>
+    {Object.values(drinks).map((d) => (
+      <DrinkInstructions drink={d} />
+    ))}
+  </RecipesContainer>
+);
